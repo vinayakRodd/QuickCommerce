c93429a3c69e077d68e95174dd255fe4cb3ceec0
vinayakRodd <vinayakrodd70@gmail.com>
2024-08-28 22:03:29 +0530

    Color Changed of Heading

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Heading() {
   return (
-    <div style={{color:'red',marginTop:'7px',fontSize:'2rem'}} >Zomato Quick Commerce</div>
+    <div style={{color:'blue',marginTop:'7px',fontSize:'2rem'}} >Zomato Quick Commerce</div>
   )
 }
 
